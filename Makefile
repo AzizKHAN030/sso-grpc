6bd5093798722edc56fc46cb1a5ecf45dca694ed
Azizbek Rasulov <khan.ak33.ak47@gmail.com>
2025-01-06 17:33:38 +0500

    Added sqlite migrations

--- Makefile
+++ Makefile
@@ -3,17 +3,24 @@ GO_CMD = go run
 MAIN_FILE = cmd/sso/main.go
 CONFIG_LOCAL = ./config/local.yaml
 CONFIG_PROD = ./config/prod.yaml
+MIGRATOR_MAIN = ./cmd/migrator
+STORAGE_PATH = ./storage/sso.db
+MIGRATIONS_PATH = ./migrations
 
 # Targets
-.PHONY: help local prod
+.PHONY: help local prod db-migrate
 
 help:
 	@echo "Usage:"
-	@echo "  make local     - Run the application with the local configuration"
-	@echo "  make prod      - Run the application with the production configuration"
+	@echo "  make local       - Run the application with the local configuration"
+	@echo "  make prod        - Run the application with the production configuration"
+	@echo "  make db-migrate  - Run database migrations"
 
 local:
 	@$(GO_CMD) $(MAIN_FILE) --config=$(CONFIG_LOCAL) || true
 
 prod:
 	@$(GO_CMD) $(MAIN_FILE) --config=$(CONFIG_PROD) || true
+
+db-migrate:
+	@$(GO_CMD) $(MIGRATOR_MAIN) --storage-path=$(STORAGE_PATH) --migrations-path=$(MIGRATIONS_PATH)
--- internal/services/auth/auth.go
+++ internal/services/auth/auth.go
@@ -37,6 +37,8 @@ type AppProvider interface {
 
 var (
 	ErrInvalidCredentials = errors.New("invalid credentials")
+	ErrInvalidAppId       = errors.New("invalid app_id")
+	ErrUserExists         = errors.New("user already exists")
 )
 
 func New(
@@ -127,6 +129,12 @@ func (a *Auth) RegisterNewUser(
 	id, err := a.userSaver.SaveUser(ctx, email, passwordHash)
 
 	if err != nil {
+		if errors.Is(err, storage.ErrUserExists) {
+			log.Warn("user already exists", sl.Err(err))
+
+			return 0, fmt.Errorf("%s: %w", op, ErrUserExists)
+		}
+
 		log.Error("failed to save user", sl.Err(err))
 
 		return 0, fmt.Errorf("%s: %w", op, err)
@@ -146,6 +154,12 @@ func (a *Auth) IsAdmin(
 	isAdmin, err := a.userProvider.IsAdmin(ctx, userId)
 
 	if err != nil {
+		if errors.Is(err, storage.ErrAppNotFound) {
+			log.Warn("user not found", sl.Err(err))
+
+			return false, fmt.Errorf("%s: %w", op, ErrInvalidAppId)
+		}
+
 		log.Error("error checking the user role")
 
 		return false, fmt.Errorf("%s: %w", op, err)
